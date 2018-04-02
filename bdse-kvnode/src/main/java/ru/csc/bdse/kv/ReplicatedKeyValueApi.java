82eafa3a06e1c7cdeed953c3cf39ea9e11e240a7
equivalence1 <equivalence1@gmail.com>
2018-04-02 21:25:35 +0300

    ReplicatedKeyValueApi: put implemented

--- bdse-kvnode/src/main/java/ru/csc/bdse/kv/ReplicatedKeyValueApi.java
+++ bdse-kvnode/src/main/java/ru/csc/bdse/kv/ReplicatedKeyValueApi.java
@@ -2,23 +2,23 @@ package ru.csc.bdse.kv;
 
 import ru.csc.bdse.util.Require;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.*;
 
 public final class ReplicatedKeyValueApi implements KeyValueApi {
 
     private final String name;
+    private final ExecutorService executor;
 
     private int WCL = 1;
     private int RCL = 1;
-    private double timeout = 0;
+    private int timeout = 0;
     private List<KeyValueApi> replics = Collections.emptyList();
 
     public ReplicatedKeyValueApi(final String name) {
         Require.nonEmpty(name, "empty name");
         this.name = name;
+        executor = Executors.newCachedThreadPool();
     }
 
     public void setWCL(int WCL) {
@@ -29,7 +29,7 @@ public final class ReplicatedKeyValueApi implements KeyValueApi {
         this.RCL = RCL;
     }
 
-    public void setTimeout(double timeout) {
+    public void setTimeout(int timeout) {
         this.timeout = timeout;
     }
 
@@ -37,7 +37,7 @@ public final class ReplicatedKeyValueApi implements KeyValueApi {
         this.replics = replics;
     }
 
-    public void configure(int WCL, int RCL, double timeout, List<KeyValueApi> replics) {
+    public void configure(int WCL, int RCL, int timeout, List<KeyValueApi> replics) {
         setWCL(WCL);
         setRCL(RCL);
         setTimeout(timeout);
@@ -46,7 +46,33 @@ public final class ReplicatedKeyValueApi implements KeyValueApi {
 
     @Override
     public void put(String key, byte[] value) {
-        throw new UnsupportedOperationException("");
+        List<Future<Integer>> replicaResults = new ArrayList<>();
+        for (KeyValueApi replica: replics) {
+            Callable<Integer> putTask = () -> {
+                try {
+                    replica.put(key, value);
+                    return 1;
+                } catch (Exception e) {
+                    return 0;
+                }
+            };
+            replicaResults.add(executor.submit(putTask));
+        }
+
+        int successfulWrites = 0;
+
+        for (Future<Integer> replicaResult: replicaResults) {
+            try {
+                successfulWrites += replicaResult.get(timeout, TimeUnit.SECONDS);
+            } catch (TimeoutException|ExecutionException|InterruptedException e) {
+                // ignore, just leave successfulWrites the same
+            }
+        }
+
+        // should we throw an exception here? `put` is void
+        if (successfulWrites < WCL) {
+            throw new RuntimeException("only got " + successfulWrites + " responses. WCL is set to " + WCL);
+        }
     }
 
     @Override
