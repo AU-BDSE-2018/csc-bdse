ac52431ed28c515083f82ccb60744f0a763049ff
equivalence1 <equivalence1@gmail.com>
2018-04-04 00:33:03 +0300

    ReplicatedKeyValueApi: get implemented, put fixed. + simple get/put test

--- bdse-kvnode/src/main/java/ru/csc/bdse/kv/replication/ReplicatedKeyValueApi.java
+++ bdse-kvnode/src/main/java/ru/csc/bdse/kv/replication/ReplicatedKeyValueApi.java
@@ -1,59 +1,47 @@
 package ru.csc.bdse.kv.replication;
 
+import com.google.protobuf.Timestamp;
 import ru.csc.bdse.kv.KeyValueApi;
 import ru.csc.bdse.kv.NodeAction;
 import ru.csc.bdse.kv.NodeInfo;
+import ru.csc.bdse.kv.serialzation.StorageSerializationUtils;
+import ru.csc.bdse.serialization.Proto;
 import ru.csc.bdse.util.Require;
 
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 public final class ReplicatedKeyValueApi implements KeyValueApi {
 
     private final String name;
     private final ExecutorService executor;
+    private final ConflictResolver conflictResolver = new ConflictResolverImpl();
 
-    private int WCL = 1;
-    private int RCL = 1;
-    private int timeout = 0;
-    private List<KeyValueApi> replics = Collections.emptyList();
+    private int WCL;
+    private int RCL;
+    private int timeout;
+    private List<KeyValueApi> replics;
 
-    public ReplicatedKeyValueApi(final String name) {
+    public ReplicatedKeyValueApi(final String name, int WCL, int RCL, int timeout, List<KeyValueApi> replics) {
         Require.nonEmpty(name, "empty name");
-        this.name = name;
         executor = Executors.newCachedThreadPool();
-    }
-
-    public void setWCL(int WCL) {
+        this.name = name;
         this.WCL = WCL;
-    }
-
-    public void setRCL(int RCL) {
         this.RCL = RCL;
-    }
-
-    public void setTimeout(int timeout) {
         this.timeout = timeout;
-    }
-
-    public void setReplics(List<KeyValueApi> replics) {
         this.replics = replics;
-    }
-
-    public void configure(int WCL, int RCL, int timeout, List<KeyValueApi> replics) {
-        setWCL(WCL);
-        setRCL(RCL);
-        setTimeout(timeout);
-        setReplics(replics);
     }
 
     @Override
     public void put(String key, byte[] value) {
-        List<Future<Integer>> replicaResults = new ArrayList<>();
+        final byte[] rawRecord = StorageSerializationUtils.serializeKeyValue(value);
+
+        final List<Future<Integer>> replicaResults = new ArrayList<>();
         for (KeyValueApi replica: replics) {
             Callable<Integer> putTask = () -> {
                 try {
-                    replica.put(key, value);
+                    replica.put(key, rawRecord);
                     return 1;
                 } catch (Exception e) {
                     return 0;
@@ -80,7 +68,65 @@ public final class ReplicatedKeyValueApi implements KeyValueApi {
 
     @Override
     public Optional<byte[]> get(String key) {
-        throw new UnsupportedOperationException("");
+        final List<Future<Integer>> replicaResults = new ArrayList<>();
+        final Map<Integer, Optional<byte[]>> returnedRawRecords = new HashMap<>();
+
+        for (int i = 0; i < replics.size(); i++) {
+            final int id = i;
+            Callable<Integer> getTask = () -> {
+                try {
+                    returnedRawRecords.put(id, replics.get(id).get(key));
+                    return 1;
+                } catch (Exception e) {
+                    return 0;
+                }
+            };
+            replicaResults.add(executor.submit(getTask));
+        }
+
+        int successfulReads = 0;
+
+        for (Future<Integer> replicaResult: replicaResults) {
+            try {
+                successfulReads += replicaResult.get(timeout, TimeUnit.SECONDS);
+            } catch (TimeoutException|ExecutionException|InterruptedException e) {
+                // ignore, just leave successfulReads the same
+            }
+        }
+
+        // should we throw an exception here? `put` is void
+        if (successfulReads < RCL) {
+            throw new RuntimeException("only got " + successfulReads + " responses. RCL is set to " + RCL);
+        }
+
+        // can't return null from collect, so need a record marking a fault
+        final Proto.RecordWithTimestamp.Builder faultRecordBuilder = Proto.RecordWithTimestamp.newBuilder();
+        faultRecordBuilder.setTimestamp(Timestamp.newBuilder().setSeconds(-1).setNanos(-1).build());
+        final Proto.RecordWithTimestamp faultRecord = faultRecordBuilder.build();
+
+        // filter out empty ones
+        final Map<Integer, Proto.RecordWithTimestamp> returnedRecords = returnedRawRecords.entrySet()
+                .stream()
+                .filter(e -> e.getValue().isPresent())
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        e -> {
+                            final byte[] rawRecord = e.getValue().orElseThrow(() -> new IllegalStateException("impossible"));
+                            try {
+                                return StorageSerializationUtils.deserializeRecord(rawRecord);
+                            } catch (Exception ex) {
+                                return faultRecord;
+                            }
+                        }))
+                .entrySet()
+                .stream()
+                .filter(e -> !e.getValue().equals(faultRecord))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        if (returnedRecords.size() == 0) {
+            return Optional.empty();
+        }
+
+        return Optional.of(conflictResolver.resolve(returnedRecords).getValue().toByteArray());
     }
 
     @Override
