46e57cca9d661518feb488fa68f1507f37b9cbbc
equivalence1 <equivalence1@gmail.com>
2018-04-04 15:38:55 +0300

    ConflicResolver: reword javadocs

--- bdse-kvnode/src/main/java/ru/csc/bdse/kv/replication/ConflictResolver.java
+++ bdse-kvnode/src/main/java/ru/csc/bdse/kv/replication/ConflictResolver.java
@@ -9,17 +9,17 @@ public interface ConflictResolver {
 
     /**
      * resolves conflicts in replications' responses on {@link ru.csc.bdse.kv.KeyValueApi#get} query
-     * @param responses map of (key, value) where @key is node's ID (= name) and @value is its response
-     * @return the record which is the real value according to the following rules:
-     *      1. Compare records' timestamps. If only one record has maximum timestamp, return it.
-     *      2. If more than one records have maximum timestamp, take the set of their values
-     *      {@link Proto.RecordWithTimestamp#getValue()} and find the most common one. Return
-     *      any record with this value.
-     *      3. If multiple different values are the most common ones (i.e. the occur the same number of times)
-     *      pick the one which was sent by the node with minimum ID.
+     * @param responses map of (key, value) where @key is node's ID and @value is its response
+     * @return the record which we suppose is the real one
      */
     Proto.RecordWithTimestamp resolve(Map<Integer, Proto.RecordWithTimestamp> responses);
 
+    /**
+     * resolves conflicts in replications' responses on {@link ru.csc.bdse.kv.KeyValueApi#getKeys} query
+     * by joining them all
+     * @param responses map of (key, value) where @key is node's ID and @value is its response
+     * @return resolved set of keys
+     */
     Set<String> resolveKeys(Map<Integer, Set<String>> responses);
 
 }
