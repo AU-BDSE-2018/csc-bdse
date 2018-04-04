0f03e091cbb7458172f7471c5be2e6b9523e26ae
equivalence1 <equivalence1@gmail.com>
2018-04-04 16:23:01 +0300

    ReplicatedKeyValueApi: getInfo and action implemented, simple tests added

--- bdse-kvnode/src/test/java/ru/csc/bdse/kv/replication/ReplicatedKeyValueApiTest.java
+++ bdse-kvnode/src/test/java/ru/csc/bdse/kv/replication/ReplicatedKeyValueApiTest.java
@@ -4,11 +4,15 @@ import org.junit.Before;
 import org.junit.Test;
 import ru.csc.bdse.kv.InMemoryKeyValueApi;
 import ru.csc.bdse.kv.KeyValueApi;
+import ru.csc.bdse.kv.NodeAction;
+import ru.csc.bdse.kv.NodeInfo;
 
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static ru.csc.bdse.kv.NodeStatus.DOWN;
+import static ru.csc.bdse.kv.NodeStatus.UP;
 
 public class ReplicatedKeyValueApiTest {
 
@@ -69,6 +73,32 @@ public class ReplicatedKeyValueApiTest {
         final Set<String> answer = new HashSet<>(Arrays.asList(key1, key2, key3, key4));
 
         assertEquals(answer, api.getKeys(prefix));
+    }
+
+    @Test
+    public void getInfoTest() {
+        final Set<NodeInfo> answer = new HashSet<>();
+        for (KeyValueApi inMemoryApi: inMemoryApis) {
+            answer.addAll(inMemoryApi.getInfo());
+        }
+
+        assertEquals(answer, api.getInfo());
+    }
+
+    @Test
+    public void actionTest() {
+        final Set<NodeInfo> answer = new HashSet<>();
+        answer.add(new NodeInfo("1", UP));
+        answer.add(new NodeInfo("2", DOWN));
+        answer.add(new NodeInfo("3", DOWN));
+
+        api.action("2", NodeAction.DOWN);
+        api.action("3", NodeAction.DOWN);
+
+        api.action("1", NodeAction.DOWN);
+        api.action("1", NodeAction.UP);
+
+        assertEquals(answer, api.getInfo());
     }
 
 }
