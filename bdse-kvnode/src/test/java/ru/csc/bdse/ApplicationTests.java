d1c74f84b44872b97233ee880f34170a41567af4
equivalence1 <equivalence1@gmail.com>
2018-04-05 23:49:45 +0300

    Accidentelly added ApplicationTest. We dont use it, so remove it.

--- bdse-kvnode/src/test/java/ru/csc/bdse/ApplicationTests.java
+++ bdse-kvnode/src/test/java/ru/csc/bdse/ApplicationTests.java
@@ -1,32 +1,16 @@
 package ru.csc.bdse;
 
+import org.junit.Ignore;
 import org.junit.Test;
-import ru.csc.bdse.kv.NodeAction;
-import ru.csc.bdse.kv.client.ControllerKeyValueApiHttpClient;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertFalse;
-
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//@Ignore
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Ignore
 public class ApplicationTests {
-
-	private static final String nodeName = "node-0";
-
 	@Test
 	public void contextLoads() {
-		ControllerKeyValueApiHttpClient api = new ControllerKeyValueApiHttpClient("http://localhost:8080");
-		api.configure(1, 1, 1);
-		api.addReplica("-");
-
-		final String key = "SomeKey";
-		final String value = "SomeValue";
-
-		api.put(key, value.getBytes());
-		api.action(nodeName, NodeAction.DOWN);
-		assertFalse(api.get(key).isPresent());
-		api.action(nodeName, NodeAction.UP);
-
-		api.delete(key);
 	}
 }
